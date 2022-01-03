56a96c4745fd1fbac9829e0153e2adcef09b0d74
Muhamad Yusup Hamdani <myhamdani1014@pnm.co.id>
2022-01-03 23:37:51 +0700

    update

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -11,6 +11,49 @@ async function fetchWithTimeout(resource, options = {}) {
     return response;
 }
 
+function fetchWithTimeoutWrapper(resource, options = {}) {
+    const { timeout = 8000 } = options;
+
+    const FETCH_TIMEOUT = timeout;
+    let didTimeOut = false;
+
+    return new Promise(function(resolve, reject) {
+        const timeout = setTimeout(function() {
+            didTimeOut = true;
+            reject(new Error('Request timed out'));
+        }, FETCH_TIMEOUT);
+
+        fetch(resource, {
+            ...options
+        })
+        .then(function(response) {
+            // Clear the timeout as cleanup
+            clearTimeout(timeout);
+            if (!didTimeOut) {
+                console.log('fetch good! ', response);
+                resolve(response);
+            }
+        })
+        .catch(function(err) {
+            console.log('fetch failed! ', err);
+
+            // Rejection already happened with setTimeout
+            if (didTimeOut) return;
+            // Reject with error
+            reject(err);
+        });
+    })
+    .then(function() {
+        // Request success and no timeout
+        console.log('good promise, no timeout! ');
+    })
+    .catch(function(err) {
+        // Error: response error, request timeout or runtime error
+        console.log('promise error! ', err);
+        reject(err);
+    });
+}
+
 const inputVal = (v, maxV, setValue) =>{
     var tl = parseInt(v);
     if(tl > maxV) return alert(`Maaf, Anda telah mencapai batas input maksimum ${maxV}.`);
@@ -40,5 +83,6 @@ export {
     capitalize,
     currency,
     digits_only,
-    replaceSpecialChar
+    replaceSpecialChar,
+    fetchWithTimeoutWrapper
 }
