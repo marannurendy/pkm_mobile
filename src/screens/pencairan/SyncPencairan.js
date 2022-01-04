8f59a5f43cccb5c64379bd7b520247ba4f75d077
barqahgenardo <barqahs@outlook.com>
2022-01-04 16:04:50 +0700

    update Pencairan

--- src/screens/pencairan/SyncPencairan.js
+++ src/screens/pencairan/SyncPencairan.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { View, Text, StyleSheet, ImageBackground, Dimensions, TouchableOpacity, TextInput, FlatList, SafeAreaView, ToastAndroid, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, Dimensions, TouchableOpacity, TextInput, FlatList, Alert, SafeAreaView, ToastAndroid, ScrollView } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
@@ -118,7 +118,13 @@ const SyncPencairan = () => {
                     .then((response) => response.json())
                     .then((responseJSON) => {
                         if (responseJSON.code === 200) {
-                            ToastAndroid.show("Sync Data berhasil!", ToastAndroid.SHORT);
+                            Alert.alert(  
+                                'Berhasil',  
+                                'Sync Data Berhasil',  
+                                [  
+                                    {text: 'OK', onPress: () => buttonFinish()},  
+                                ]
+                            )  
                             if (__DEV__) console.log('doSubmitPencairan db.transaction insert/update success');
                             for(let a = 0; a < ah.length; a++){
                                 const queryDeleteSosialisasiDatabase = 'DELETE FROM Table_Pencairan_Post where ID_Prospek = "'+ ah[a].ID_Prospek +'"';
@@ -134,17 +140,24 @@ const SyncPencairan = () => {
                                 );
                             }
                             return true;
+                        }else{
+                            Alert.alert(  
+                                'Gagal!',  
+                                'Sync Data Tidak Berhasil',  
+                                [  
+                                    {text: 'OK', onPress: () => buttonFinish()},  
+                                ]
+                            )  
                         }
                     })
                 })
             }
         )
-        buttonFinish();
     }
 
     const buttonFinish = async () => {
         setLoaded(false)
-        navigation.replace('FrontHome')
+        navigation.replace('Pencairan')
     }
 
     // LIST VIEW PENCAIRAN
