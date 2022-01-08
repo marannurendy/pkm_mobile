3119cc213439c9606413e97ee5a0e90a6d8157ee
Muhamad Yusup Hamdani <myhamdani1014@pnm.co.id>
2022-01-08 18:48:26 +0700

    update

--- src/screens/HomeSync.js
+++ src/screens/HomeSync.js
@@ -868,7 +868,7 @@ export default function FrontHomeSync(props) {
 
     const renderVersion = () => (
         <View style={{ marginVertical: 8 }}>
-            <Text style={{ textAlign: 'center' }}>version pkm_mobile-0.0.1-055-dev @ 2021-01-08</Text>
+            <Text style={{ textAlign: 'center' }}>version pkm_mobile-0.0.1-056-dev @ 2021-01-08</Text>
         </View>
     )
 
