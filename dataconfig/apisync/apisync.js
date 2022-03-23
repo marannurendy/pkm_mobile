98e2e596512c9e54ad801f6cbac908e15ab83bd1
TIF - Muhamad Yusup Hamdani (YPH) <myhamdani1014@pnm.co.id>
2022-03-23 22:26:24 +0700

    update

--- dataconfig/apisync/apisync.js
+++ dataconfig/apisync/apisync.js
@@ -15,7 +15,7 @@ let MAJOR_VERSION = '0';
 let MINOR_VERSION = '0';
 let PATCH_VERSION = '3';
 let BUILD = '003';
-let TANGGAL = '2022-03-19';
+let TANGGAL = '2022-03-23';
 
 let base_url = 'http://pkmmekaar.kresnasaraswati.id';
 let api_version = 'v1';
@@ -23,8 +23,8 @@ let api_version = 'v1';
 if (IS_DEVELOPMENT) {
     MAJOR_VERSION = '0';
     MINOR_VERSION = '0';
-    PATCH_VERSION = '2';
-    BUILD = '026';
+    PATCH_VERSION = '3';
+    BUILD = '005';
     
     base_url = 'http://103.105.216.134';
 }
--- src/screens/HomeSync.js
+++ src/screens/HomeSync.js
@@ -904,6 +904,7 @@ export default function FrontHomeSync(props) {
     const renderVersion = () => (
         <View style={{ marginVertical: 8 }}>
             <Text style={{ textAlign: 'center' }}>{`version pkm_mobile-${VERSION}`}</Text>
+            <Text style={{ textAlign: 'center' }} onPress={() => navigation.navigate('NetworkLogging')}>Network Logger</Text>
         </View>
     )
 
