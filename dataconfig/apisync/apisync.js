d6751f50b5704de3ffe3eb44f19ca6f49e31ccec
TIF - Muhamad Yusup Hamdani (YPH) <myhamdani1014@pnm.co.id>
2022-03-25 23:02:47 +0700

    relase 0.0.5-001-prod

--- dataconfig/apisync/apisync.js
+++ dataconfig/apisync/apisync.js
@@ -9,11 +9,11 @@ import {View, Texr} from "react-native"
 //let ApiSync = 'http://10.50.0.34:9005/' //development
 // let ApiSync = 'http://192.168.233.159:9005/'
 
-const IS_DEVELOPMENT = true;
+const IS_DEVELOPMENT = false;
 
 let MAJOR_VERSION = '0';
 let MINOR_VERSION = '0';
-let PATCH_VERSION = '4';
+let PATCH_VERSION = '5';
 let BUILD = '001';
 let TANGGAL = '2022-03-25';
 
